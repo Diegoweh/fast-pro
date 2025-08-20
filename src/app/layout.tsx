dcfa73a0a62dabb358b428c5fef0c45e775578d5
Diegoweh <105457980+Diegoweh@users.noreply.github.com>
2025-08-20 15:49:58 -0700

    tawk.to script added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,22 @@ export default function RootLayout({
             },
           ]} />
         {children}
+        {/* Tawk.to Live Chat Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/67d1e3da8ebbe7190b12cc58/1im5u4322';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
         <Footer />
       </body>
     </html>
